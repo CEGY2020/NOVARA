e17e64c08939e4ab18ccf5e168f1ef2c5cdeaf3e
CEGY2020 <61029622+CEGY2020@users.noreply.github.com>
2026-08-02 13:56:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ document.querySelector("form").addEventListener("submit", function (e) {
     // TODO: Replace this with AWS Cognito authentication.
     // For now, continue to the dashboard.
     window.location.href = "dashboard.html";
+});
