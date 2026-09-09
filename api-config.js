d74e19ce78c29520fd144058b4e6f2fd2ece4a36
CEGY2020 <61029622+CEGY2020@users.noreply.github.com>
2026-09-09 14:13:00 -0700

    Point platform to current NOVARA API Gateway

--- api-config.js
+++ api-config.js
@@ -1,4 +1,4 @@
-window.NOVARA_API_BASE = "https://66yy88811j.execute-api.us-west-2.amazonaws.com";
+window.NOVARA_API_BASE = "https://jwhirgzy45.execute-api.us-west-2.amazonaws.com";
 
 (function () {
   if (!document.body || document.body.getAttribute("data-page") !== "leads") return;
