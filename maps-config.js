4b2388d12a7b70f4989eca9f26732fb289e1994a
CEGY2020 <61029622+CEGY2020@users.noreply.github.com>
2026-09-13 12:27:10 -0700

    Update maps-config.js

--- maps-config.js
+++ maps-config.js
@@ -2,4 +2,4 @@
  * Add the browser-restricted Google Maps Platform key here after it is restricted
  * to the NOVARA production site and the required Google Maps/Places APIs.
  */
-window.NOVARA_GOOGLE_MAPS_API_KEY = window.NOVARA_GOOGLE_MAPS_API_KEY || "";
+window.NOVARA_GOOGLE_MAPS_API_KEY = window.NOVARA_GOOGLE_MAPS_API_KEY || "AIzaSyBsOnWyTS3AkTJn1erQ0HSoPoLbzvRxxAI";
